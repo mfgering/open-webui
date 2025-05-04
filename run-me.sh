01f2af8fcab4d85fdc499addbbd285ba251823ba
mike <mike@aol.com>
2025-05-04 11:32:16 -0400

    x

--- run-me.sh
+++ run-me.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
-#NOTE: the .env file has API keys, etc
-if [ "$1" == "" ]; then
-    OP="up"
-else
-    OP="$1"
-fi
+# NOTE: the .env file has API keys, etc
 
-source .env && docker compose -f docker-compose.yaml -f docker-compose.amdgpu.yaml -f docker-compose.api.yaml -f docker-compose.mfg.yaml $OP
+# Default operation
+OP="up"
+
+# Process arguments
+while (( "$#" )); do
+  case "$1" in
+    -d)
+      DETACH="--detach"
+      shift
+      ;;
+    *)
+      # Assume it's the operation if not a known flag
+      OP=$1
+      shift
+      ;;
+  esac
+done
+
+# Load environment variables
+source .env
+
+# Execute Docker Compose command
+docker compose -f docker-compose.yaml -f docker-compose.amdgpu.yaml -f docker-compose.api.yaml -f docker-compose.mfg.yaml $OP $DETACH
